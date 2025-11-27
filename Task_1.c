98e2ac5465a4fb878c8b8beba13dc0047fb1ec86
Atif-2602 <atifq2602@eng.rizvi.edu.in>
2025-11-27 17:08:24 +0530

    Add files via upload

---
+++ Task_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    // My Name
+    printf( "Name :Qureshi Atif Meraj Alam \n");
+
+    // My age
+    printf("Age : 18 \n");
+
+    // My Class
+    printf("Class: F.E \n");
+
+    // My Division
+    printf("Division: C \n");
+
+    // My UIN
+    printf("Uin: 251P004 \n");
+
+    return 0;
+
+}
