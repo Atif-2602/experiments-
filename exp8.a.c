997191dd3f7eb886828240aff76ce7c8df8ac693
Atif-2602 <atifq2602@eng.rizvi.edu.in>
2025-11-27 20:39:37 +0530

    Add files via upload

---
+++ exp8.a.c
@@ -0,0 +1,21 @@
+//name:Qureshi Atif Meraj Alam
+//uin:251P004
+//class:F.E
+//div:c
+#include <stdio.h>
+
+int stringLength(char str[]) {
+    int len = 0;
+    while (str[len] != '\0')
+        len++;
+    return len;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    gets(str); // or use fgets(str, 100, stdin);
+
+    printf("Length of string: %d\n", stringLength(str));
+    return 0;
+}
